028f0c99f95d5ae745778490d5fc5c2782ea50ef
shwetachari <shwetachari@gmail.com>
2017-09-08 16:23:29 -0700

    rendered all components of App and update videoPlaying on click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,44 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      videoList: window.exampleVideoData,
+      videoPlaying: window.exampleVideoData[0]
+    }
+  }
+
+  render() {
+    console.log('videoPlaying', this.state.videoPlaying);
+
+
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search/>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.videoPlaying}/>
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videoList} func={this.handleClick} parent={this}/>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    )
+  }
+
+  handleClick(parent) {
+    console.log('parent', parent);
+    console.log('this', this);
+    parent.setState({
+      videoPlaying: this
+    });
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,20 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+class VideoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="video-list">
+        {this.props.videos.map((video, index) => (
+          <VideoListEntry func={this.props.func} parent={this.props.parent} key={index} video={video}/>
+        ))}
+      </div>
+    );
+  }
+
+
+}
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
